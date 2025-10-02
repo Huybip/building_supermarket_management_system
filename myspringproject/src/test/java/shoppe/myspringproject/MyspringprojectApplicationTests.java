8326fc6879b90f2f1a005163eb83816af8bcc56c
Huybip <155954088+Huybip@users.noreply.github.com>
2025-10-02 15:57:15 +0700

    Add files via upload

---
+++ myspringproject/src/test/java/shoppe/myspringproject/MyspringprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package shoppe.myspringproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyspringprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
