1e1eb918d3f1b51cc0aef598dc0e1b57c6872266
Huybip <155954088+Huybip@users.noreply.github.com>
2025-10-23 11:48:04 +0000

    main test

--- spring-boot/complete/src/test/java/com/example/servingwebcontent/MainTest.java
+++ spring-boot/complete/src/test/java/com/example/servingwebcontent/MainTest.java
@@ -27,7 +27,7 @@ import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.test.web.servlet.MockMvc;
 
 @WebMvcTest(controllers = GreetingController.class)
-public class ServingWebContentApplicationTest {
+public class MainTest {
 
 	@Autowired
 	private MockMvc mockMvc;
